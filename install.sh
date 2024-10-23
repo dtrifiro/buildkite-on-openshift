f0825e42deb0d7b7277b2bf4fcb7d1670a3db090
Daniele Trifirò <dtrifiro@redhat.com>
2024-10-23 12:16:20 +0200

    🐶: remove revoked credentials

--- install.sh
+++ install.sh
@@ -8,11 +8,6 @@ fi
 
 source env
 
-export AGENT_TOKEN=rRxxLdcZpA4jTCYaERE9NHKi
-export GRAPHQL_TOKEN=bkua_e89904377fa7781750b89c620177cf2938daf7ba
-export ORGANIZATION=dtrifiro-testing
-export CLUSTER_UUID=1d4c27a6-ae3c-4e11-bec8-c154e555c769
-
 helm upgrade --install agent-stack-k8s oci://ghcr.io/buildkite/helm/agent-stack-k8s \
 	--create-namespace \
 	--namespace buildkite \
